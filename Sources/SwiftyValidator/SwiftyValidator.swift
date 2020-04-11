6e5d47baa6491c190f4e6ef13d9c4b6517d2447f
kiarash.avin <kiarash.kiani@avinance-media.com>
2020-04-11 11:41:12 +0430

    fixed to match whole string

--- Sources/SwiftyValidator/SwiftyValidator.swift
+++ Sources/SwiftyValidator/SwiftyValidator.swift
@@ -2,10 +2,10 @@ import Foundation
 
 public extension String{
     func validate(_ pattern: String) -> Bool {
-        guard (try? NSRegularExpression(pattern: pattern, options: []).firstMatch(in: self, options: [], range: NSRange(location: 0, length: self.count))) != nil else{
+        guard let match = (try? NSRegularExpression(pattern: pattern, options: []).firstMatch(in: self, options: [], range: NSRange(location: 0, length: self.count)))else{
             return false
         }
         
-        return true
+        return match.range == NSRange(location: 0, length: self.count)
     }
 }
